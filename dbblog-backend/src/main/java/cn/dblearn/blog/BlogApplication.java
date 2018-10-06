e9a46772ed9c4cdff364a07c5c9c2257b69806a1
llldddbbb <571002217@qq.com>
2018-10-06 15:16:41 +0800

    init blog backend

---
+++ dbblog-backend/src/main/java/cn/dblearn/blog/BlogApplication.java
@@ -0,0 +1,12 @@
+package cn.dblearn.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
