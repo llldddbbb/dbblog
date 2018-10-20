29b27eb58fc3ed467f9baf237815b360d63296fd
llldddbbb <571002217@qq.com>
2018-10-20 17:15:17 +0800

    添加跨域配置

---
+++ dbblog-backend/src/main/java/cn/dblearn/blog/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package cn.dblearn.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CorsConfig
+ *
+ * @author bobbi
+ * @date 2018/10/20 17:12
+ * @email 571002217@qq.com
+ * @description 跨域配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
