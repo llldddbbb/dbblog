bf7b53459953b7a529e95f457bd427d717ef825e
llldddbbb <571002217@qq.com>
2018-10-23 17:05:06 +0800

    修复svg-icon显示问题

--- dbblog-manage-frontend/build/webpack.base.conf.js
+++ dbblog-manage-frontend/build/webpack.base.conf.js
@@ -52,7 +52,12 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')]
+      },
+      {
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
--- dbblog-manage-frontend/src/views/main-navbar.vue
+++ dbblog-manage-frontend/src/views/main-navbar.vue
@@ -2,8 +2,8 @@
   <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-        <a class="site-navbar__brand-lg" href="javascript:;">人人快速开发平台</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">人人</a>
+        <a class="site-navbar__brand-lg" href="javascript:;">db's Blog</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">DB</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
@@ -101,7 +101,7 @@ export default {
           method: 'post',
           data: this.$http.adornData()
         }).then(({data}) => {
-          if (data && data.code === 0) {
+          if (data && data.code === 200) {
             clearLoginInfo()
             this.$router.push({ name: 'login' })
           }
