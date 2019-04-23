d79890a39c81ea17582b13284a8f0485430068a7
llldddbbb <571002217@qq.com>
2019-04-23 19:35:36 +0800

    fix[sql]:更新开发环境账号为admin，密码为123456

--- dbblog-backend/db/dbblog_sys_user.sql
+++ dbblog-backend/db/dbblog_sys_user.sql
@@ -1,1 +1,3 @@
-INSERT INTO dbblog.sys_user (user_id, username, password, email, salt, create_user_id, create_time, status) VALUES (1, 'admin', '42f60d79943bb3e282c5fb630c7bbba682f7d95768a4f8cea0f8ab57f16bcf12', '571002217@qq.com', 'YzcmCZNvbXocrsz9dm8e', '1', '2019-03-04 23:20:16', 1);
+# ¸üĞÂÕËºÅÃÜÂëÎªadmin£¬123456
+
+INSERT INTO dbblog.sys_user (user_id, username, password, email, salt, create_user_id, create_time, status) VALUES (1, 'admin', 'cdac762d0ba79875489f6a8b430fa8b5dfe0cdd81da38b80f02f33328af7fd4a', '571002217@qq.com', 'YzcmCZNvbXocrsz9dm8e', '1', '2019-04-23 19:17:00', 1);
