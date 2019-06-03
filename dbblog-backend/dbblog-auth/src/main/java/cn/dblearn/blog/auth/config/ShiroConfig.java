cf8d01c89d1026ab31d573a4856d90cc93e3822b
songyucai <songyucai@vipkid.com.cn>
2019-06-03 18:49:13 +0800

    change es properties;

--- dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/config/ShiroConfig.java
+++ dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/config/ShiroConfig.java
@@ -1,4 +1,5 @@
 package cn.dblearn.blog.auth.config;
+
 import cn.dblearn.blog.auth.OAuth2Filter;
 import cn.dblearn.blog.auth.OAuth2Realm;
 import org.apache.shiro.session.mgt.SessionManager;
@@ -7,10 +8,12 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.elasticsearch.client.Client;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.shiro.mgt.SecurityManager;
+
 import javax.servlet.Filter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -29,16 +32,26 @@ import java.util.Map;
 public class ShiroConfig {
 
     @Bean("sessionManager")
-    public SessionManager sessionManager(){
+    public SessionManager sessionManager() {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
         // 是否定时检查session
         sessionManager.setSessionValidationSchedulerEnabled(false);
         return sessionManager;
     }
 
+    /**
+     * Client 注入是为了解决 System.setProperty("es.set.netty.runtime.available.processors","false")这个问题  提前初始化ES Client
+     * 否则先初始化oAuth2Realm的时候，会把RedisClient内使用netty初始化，从而导致报错~~~
+     * 根本原因：es客户端的初始化bug。另外需要注意isTypeMatch()问题导致的误伤
+     *
+     * @param client         client
+     * @param oAuth2Realm    oAuth2Realm
+     * @param sessionManager sessionManager
+     * @return
+     */
     @Bean("securityManager")
-    public SecurityManager securityManager(OAuth2Realm oAuth2Realm, SessionManager sessionManager){
-        DefaultWebSecurityManager securityManager=new DefaultWebSecurityManager();
+    public SecurityManager securityManager(Client client, OAuth2Realm oAuth2Realm, SessionManager sessionManager) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(oAuth2Realm);
         securityManager.setSessionManager(sessionManager);
         return securityManager;
--- dbblog-backend/dbblog-search/src/main/java/cn/dblearn/blog/BlogApplication.java
+++ dbblog-backend/dbblog-search/src/main/java/cn/dblearn/blog/BlogApplication.java
@@ -17,10 +17,6 @@ public class BlogApplication {
 
 
     public static void main(String[] args) {
-        /**
-         * ElasticSearch 所需的临时设置，待解决
-         */
-        System.setProperty("es.set.netty.runtime.available.processors","false");
         SpringApplication.run(BlogApplication.class, args);
     }
 }
